900a2a4ed46408cb23c7fce440c365d21757c93a
F4LC0NN <thomasruben25@gmail.com>
2022-02-26 19:20:54 +0100

    Create basic operations & an operation controller

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/* 
+--------------------------------------------------
+TESTS FOR CALCULI
+--------------------------------------------------
+*/
+
+function add(num) {
+  num = [...arguments];
+  return num.reduce((a, b) => a + b, 0);
+}
+
+function substract(num) {
+  num = [...arguments];
+  return num.reduce((a, b) => b - a, 0);
+}
+
+function multiply(num) {
+  num = [...arguments];
+  return num.reduce((a, b) => a * b);
+}
+
+function divide(num) {
+  num = [...arguments];
+  return num.reduce((a, b) => (b === 0) ? 'ERROR - 0 DIVISOR' : Number((b / a).toFixed(2)));
+}
+
+function operate(operator, firstOperand, secondOperand) {
+  switch (operator) {
+    case '+':
+      return add(firstOperand, secondOperand);
+    case '-':
+      return substract(firstOperand, secondOperand);
+    case '*':
+      return multiply(firstOperand, secondOperand);
+    case '/':
+      return divide(firstOperand, secondOperand);
+  }
+}
+
+// Consoles for tests
+console.log(add(1, 2));
+console.log(substract(1, 6));
+console.log(multiply(100, 100));
+console.log(divide(3, 1));
+console.log(operate('+', 4, 3))

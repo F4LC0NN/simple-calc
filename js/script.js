37dc4ea790726abf2c552fb0da97234e6167599c
F4LC0NN <thomasruben25@gmail.com>
2022-03-02 14:13:28 +0100

    Create operation methods to facilitate their access in the future

--- js/script.js
+++ js/script.js
@@ -10,4 +10,53 @@ class Calculator {
     this.firstOperand = firstOperand;
     this.secondOperand = secondOperand;
   }
+
+  get addition() {
+    return this.sum();
+  }
+
+  get substraction() {
+    return this.difference();
+  }
+
+  get multiplication() {
+    return this.product();
+  }
+ 
+  get division() {
+    return this.quotient();
+  }
+  
+  get percentage() {
+    return this.percent;
+  }
+
+  get exponentiation() {
+    return this.power();
+  }
+
+
+  sum() {
+    return Math.round((this.firstOperand + this.secondOperand) * 10) / 10;
+  }
+
+  difference() {
+    return Math.round((this.firstOperand - this.secondOperand) * 10) / 10;
+  }
+
+  product() {
+    return Math.round((this.firstOperand * this.secondOperand) * 10) / 10;
+  }
+
+  quotient() {
+    return Math.round((this.firstOperand / this.secondOperand) * 100) / 100;
+  }
+
+  percent() {
+    return Math.round((this.firstOperand / 100) * 100) / 100;
+  }
+
+  power() {
+    return Math.round((this.firstOperand ** this.secondOperand).toExponential(2));
+  }
 }

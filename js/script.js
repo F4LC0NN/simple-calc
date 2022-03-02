70ccb096cb03e6b9bc593fa15f068090c7909524
F4LC0NN <thomasruben25@gmail.com>
2022-03-02 14:09:47 +0100

    Update js file. Build a calculator object

--- js/script.js
+++ js/script.js
@@ -1,45 +1,13 @@
-/* 
---------------------------------------------------
-TESTS FOR CALCULI
---------------------------------------------------
+/*
+---------------------------------------------------------------------
+CALCULATOR OBJECT
+---------------------------------------------------------------------
 */
 
-function add(num) {
-  num = [...arguments];
-  return num.reduce((a, b) => a + b, 0);
-}
-
-function substract(num) {
-  num = [...arguments];
-  return num.reduce((a, b) => b - a, 0);
-}
-
-function multiply(num) {
-  num = [...arguments];
-  return num.reduce((a, b) => a * b);
-}
-
-function divide(num) {
-  num = [...arguments];
-  return num.reduce((a, b) => (a === 0) ? 'ERROR - 0 DIVISOR' : Number((b / a).toFixed(2)));
-}
-
-function operate(operator, firstOperand, secondOperand) {
-  switch (operator) {
-    case '+':
-      return add(firstOperand, secondOperand);
-    case '-':
-      return substract(firstOperand, secondOperand);
-    case '*':
-      return multiply(firstOperand, secondOperand);
-    case '/':
-      return divide(firstOperand, secondOperand);
+// Create a calculator to control basic operations through defined operands
+class Calculator {
+  constructor(firstOperand, secondOperand) {
+    this.firstOperand = firstOperand;
+    this.secondOperand = secondOperand;
   }
 }
-
-// Consoles for tests
-console.log(add(1, 2));
-console.log(substract(1, 6));
-console.log(multiply(100, 100));
-console.log(divide(3, 1));
-console.log(operate('+', 0, 3))
